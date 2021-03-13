8308a38e3c900f2789209c7860e7349f737d6ff3
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-03-13 11:23:22 -0600

    Descargando información desde una API y creando nodos

--- src/index.js
+++ src/index.js
@@ -3,4 +3,30 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo-nine.vercel.app/api/avo"
+
+// Web api
+// Conectarnos al server
+window
+  .fetch(url)
+  // Pocesar la resouesta y convertirla en JSON
+  .then(response => response.json())
+  // JSON -> Data -> Renderizar info in Browser
+  .then(responseJson => {
+    const allItems = []
+    responseJson.data.forEach((item) => {
+      // Crear imagen
+      const imagen = document.createElement('img')
+      // Crear titulo
+      const title = document.createElement('h2')
+      // Crear precio
+      const price = document.createElement('div')
+      
+      const container = document.createElement('div')
+      container.append(imagen, title, price)
+
+      allItems.push(container)
+    })
+
+    document.body.append(...allItems)
+  })
